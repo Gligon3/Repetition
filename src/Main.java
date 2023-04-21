53f415df1bdf6dc9ca0d8910e4120885a90d5aee
Gligon3 <94972508+Gligon3@users.noreply.github.com>
2023-04-21 11:10:54 +0200

    Gjorde den första uppgiften.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,28 @@
+import java.awt.image.BufferedImage;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.lang.reflect.Array;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        readLogfile("ideaold.log");
+
     }
-}
+        public static ArrayList<String> readLogfile(String filePath) {
+            ArrayList<String> loglines = new ArrayList<>();
+
+            try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    loglines.add(line);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return loglines;
+
+        }
+    }
